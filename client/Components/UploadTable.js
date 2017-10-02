d08c48110019d8b9764b424b9bd4c4e72ad6f235
Anna <anna@Annas-MacBook-Pro.local>
2017-10-02 19:45:38 +1100

    Added needed components.

--- client/Components/UploadTable.js
+++ client/Components/UploadTable.js
@@ -1,16 +1,12 @@
 import React from 'react';
-// import styles from './App.css';
 
 export default class Upload extends React.Component {
-    // constructor(props) {
-    //     super(props);
-    //     this.state = { test: 'foo' };
-    // }
+
     render() {
         return (
-            <form>
-                <label htmlFor="file">Choose file to upload</label>
-                <input type="file" id="file" name="file" multiple />
+            <form action="/rest/expenses/upload">
+                <label htmlFor="file">Choose .csv file to upload</label>
+                <input type="file" id="file" className="" name="file" multiple />
                 <button>Submit</button>
             </form>
         );
