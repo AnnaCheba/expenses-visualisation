9cdb2695a1e49f070f2bfde55ff7777e87451ed6
Anna <anna@Unknown.gateway>
2017-10-18 14:42:10 +1100

    Removed not needed files.

--- server/get-iems.js
+++
@@ -1,9 +0,0 @@
-const db = require('./db');
-
-module.exports = fileId => new Promise((resolve) => {
-    console.log('fileId', fileId);
-
-    db.all(`SELECT * from items WHERE file_id = '${fileId}'`, (err, rows) => {
-        resolve(rows);
-    });
-});
